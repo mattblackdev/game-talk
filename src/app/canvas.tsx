6592a5224cf821ffa1f99ef41887c418adb9d3a7
Matt Black <matt@mattblack.dev>
2025-01-21 13:27:49 -0500

    Clean up

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,25 +1,6 @@
 import { Canvas as R3fCanvas } from '@react-three/fiber'
 import { ReactNode } from 'react'
 
-// import { getProject } from '@theatre/core'
-// import { editable as e, PerspectiveCamera, SheetProvider } from '@theatre/r3f'
-// import extension from '@theatre/r3f/dist/extension'
-// import studio from '@theatre/studio'
-// import demoProjectState from '../cube.json'
-
-// studio.initialize()
-// studio.extend(extension)
-
-// const demoSheet = getProject('Demo Project', {
-//   state: demoProjectState,
-// }).sheet('Demo Sheet')
-
-// useEffect(() => {
-//   demoSheet.project.ready.then(() =>
-//     demoSheet.sequence.play({ iterationCount: Infinity }),
-//   )
-// }, [])
-
 export function Canvas({ children }: { children: ReactNode }) {
   return (
     <R3fCanvas
